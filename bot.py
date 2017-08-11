6192435e3a722fb2f7ac1939cae5b98cbf23b2dc
Mitch Weaver <mitchweaver@protonmail.com>
2017-08-10 23:48:24 -0500

    initial commit

---
+++ bot.py
@@ -0,0 +1,19 @@
+import discord
+from discord.ext import commands
+#import logging
+#import asyncio
+
+des = 'This is the description inside my code file!'
+
+prefix = '!'
+
+client = commands.Bot(description=des, command_prefix=prefix)
+
+@client.event
+async def on_ready();
+
+
+
+
+
+#MzQ1NDAwODA0OTY4MTAzOTM3.DG676w.gt_HkXfpCQbxuEwoiHGACywn5Bs
