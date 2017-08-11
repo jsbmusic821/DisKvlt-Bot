4d28c2cfa22bb50889c09b5262e20f579231896e
Mitch Weaver <mitchweaver@protonmail.com>
2017-08-11 00:31:07 -0500

    commit

--- bot.py
+++ bot.py
@@ -1,19 +1,22 @@
 import discord
 from discord.ext import commands
-#import logging
+from discord.ext import asyncio
+from discord.ext import async
+import logging
 #import asyncio
 
 des = 'This is the description inside my code file!'
 
 prefix = '!'
 
-client = commands.Bot(description=des, command_prefix=prefix)
+client = commands.Bot(description=des, command_prefix=prefix);
 
 @client.event
 async def on_ready();
+    print("Hello world!")
 
+@client.command(pass_context=True)
+async def ping(ctx):
+    await client.say('pong')
 
-
-
-
-#MzQ1NDAwODA0OTY4MTAzOTM3.DG676w.gt_HkXfpCQbxuEwoiHGACywn5Bs
+client.run('MzQ1NDAwODA0OTY4MTAzOTM3.DG676w.gt_HkXfpCQbxuEwoiHGACywn5Bs')
