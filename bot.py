18099760680174c2da153c57f790ca3677543e4e
Mitch Weaver <mitchweaver@protonmail.com>
2017-08-11 16:03:55 -0500

    added lyric fetching

--- bot.py
+++ bot.py
@@ -46,7 +46,7 @@ async def lyrics(ctx,args):
     arr = '{}'.format(args).split(' - ')
     lyrics = lyricfetcher.get_lyrics('lyricswikia', arr[0], arr[1])
     if lyrics is None or lyrics == 404 or lyrics == '404':
-        await client.say('Not found. *Format:* `"Artist - Song"`')
+        await client.say('/shrug Not found. *Format:* "Artist - Song"')
     else: await client.say('```' + lyrics + '```')
 
 # COIN FLIP
