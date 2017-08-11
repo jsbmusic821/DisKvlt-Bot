05800b01f0b10ce047b152446431b21f2343a95a
Mitch Weaver <mitchweaver@protonmail.com>
2017-08-11 01:10:58 -0500

    commit

--- bot.py
+++ bot.py
@@ -2,6 +2,7 @@ import discord
 from discord.ext import commands
 import logging
 import asyncio
+import random
 
 des = 'This is the description inside my code file!'
 
@@ -16,5 +17,10 @@ async def on_ready():
 @client.command(pass_context=True)
 async def ping(ctx):
     await client.say('pong')
+
+async def coinflip(ctx):
+    if random.randint(1) == 0:
+        await client.say('Heads')
+    else: await.client.say('Tails')
 
 client.run('MzQ1NDAwODA0OTY4MTAzOTM3.DG676w.gt_HkXfpCQbxuEwoiHGACywn5Bs')
