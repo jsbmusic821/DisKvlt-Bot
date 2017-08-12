403305d99a5792d9af29b330acf5623aaa5c76cc
Mitch Weaver <mitchweaver@protonmail.com>
2017-08-11 22:00:26 -0500

    message

--- bot.py
+++ bot.py
@@ -13,9 +13,9 @@ from translate import Translator
 
 des = "Hi, I'm /r/TapeKvlt's bot! Beep, bop, boop..."
 prefix = '!'
-client = commands.Bot(description=des, command_prefix=prefix);
+bot = commands.Bot(description=des, command_prefix=prefix);
 
-@client.event
+@bot.event
 async def on_ready():
     print("~~~~~~~ Bot is starting... ~~~~~~~~~~~~")
 
@@ -24,123 +24,123 @@ async def on_ready():
 ################### END FUNCTIONS ##############################################
 
 # Server Welcome
-@client.event
+@bot.event
 async def on_member_join(member):
     server = member.server
     fmt = 'Everybody welcome {0.mention} to the server!'
-    await client.send_message(server, fmt.format(member, server))
+    await bot.send_message(server, fmt.format(member, server))
 
 ########### Keeping for historical purposes #######################
 # PING
-@client.command(pass_context=True)
+@bot.command(pass_context=True)
 async def ping(ctx):
-    await client.say('pong')
+    await bot.say('pong')
 # PONG... lulz
-@client.command(pass_context=True)
+@bot.command(pass_context=True)
 async def pong(ctx):
-    await client.say('Hey, stop that.')
+    await bot.say('Hey, stop that.')
 ###################################################################
 
 # LYRIC FETCHER
-@client.command(pass_context=True)
+@bot.command(pass_context=True)
 async def lyrics(ctx,args):
     arr = '{}'.format(args).split(' - ')
     lyrics = lyricfetcher.get_lyrics('lyricswikia', arr[0], arr[1])
     if lyrics is None or lyrics == 404 or lyrics == '404':
-        await client.say('Not found. ¯\_(ツ)_/¯ *Format:* `"Artist - Song"`')
-    else: await client.say('```' + lyrics + '```')
+        await bot.say('Not found. ¯\_(ツ)_/¯ *Format:* `"Artist - Song"`')
+    else: await bot.say('```' + lyrics + '```')
 
 # Translator
-@client.command(pass_context=True)
+@bot.command(pass_context=True)
 async def trans(ctx, args, message):
     arr = '{}'.format(args).split('->')
     t = Translator(from_lang=arr[0],to_lang=arr[1])
-    await client.say('```' + t.translate(message) + '```')
+    await bot.say('```' + t.translate(message) + '```')
 
 # COIN FLIP
-@client.command(pass_context=True)
+@bot.command(pass_context=True)
 async def coinflip(ctx):
-    await client.say('Heads') if random.randint(0, 1) else await client.say('Tails')
+    await bot.say('Heads') if random.randint(0, 1) else await bot.say('Tails')
 
-@client.command()
+@bot.command()
 async def joined(member : discord.Member):
 ##  Says the date when a member joined.
-    await client.say('{0.name} joined in {0.joined_at}'.format(member))
+    await bot.say('{0.name} joined in {0.joined_at}'.format(member))
 
 #################### WEBSITE SEARCHERS #################################
 # Wiki command
-@client.command(pass_context=True)
+@bot.command(pass_context=True)
 async def wiki(ctx,args):
-    await client.say('https://en.wikipedia.org/wiki/{}'.format(args.replace(' ', '_')))
+    await bot.say('https://en.wikipedia.org/wiki/{}'.format(args.replace(' ', '_')))
 
 # Metal Archives command
-@client.command(pass_context=True)
+@bot.command(pass_context=True)
 async def metal(ctx,args):
     temp = '{}'.format(args.replace(' ', '+'))
-    await client.say('https://www.metal-archives.com/search?searchString=' + temp + '&type=band_name'.format(args))
+    await bot.say('https://www.metal-archives.com/search?searchString=' + temp + '&type=band_name'.format(args))
 
 # YouTube command
-@client.command(pass_context=True)
+@bot.command(pass_context=True)
 async def yt(ctx,args):
-    await client.say('https://www.youtube.com/results?search_query={}'.format(args.replace(' ', '+')))
+    await bot.say('https://www.youtube.com/results?search_query={}'.format(args.replace(' ', '+')))
 
 # Discogs command
-@client.command(pass_context=True)
+@bot.command(pass_context=True)
 async def discogs(ctx,args):
     temp = '{}'.format(args.replace(' ', '+'))
-    await client.say('https://www.discogs.com/search?q=' + temp + '&btn=&type=all'.format(args))
+    await bot.say('https://www.discogs.com/search?q=' + temp + '&btn=&type=all'.format(args))
 
 # Bandcamp-search command
-@client.command(pass_context=True)
+@bot.command(pass_context=True)
 async def bcsearch(ctx,args):
-    await client.say('https://bandcamp.com/search?q={}'.format(args.replace(' ', '+')))
+    await bot.say('https://bandcamp.com/search?q={}'.format(args.replace(' ', '+')))
 
 # Bandcamp command
-@client.command(pass_context=True)
+@bot.command(pass_context=True)
 async def bc(ctx,args):
     temp = '{}'.format(args.replace(' ', ''))
-    await client.say('https://' + temp + '.bandcamp.com'.format(args))
+    await bot.say('https://' + temp + '.bandcamp.com'.format(args))
 
 ##################### END WEBSITE SEARCHERS #############################
 
 
 ################################### MEMES #######################################
 # CRISPY
-@client.command(pass_context=True)
+@bot.command(pass_context=True)
 async def crispy(ctx):
     i = random.randint(0, 2)
-    if i == 0: await client.say('https://i.imgur.com/YVfXE7W.gif')
-    elif i == 1: await client.say('https://i.imgur.com/2SRtfz5.jpg')
-    elif i == 2: await client.say('https://i.imgur.com/TiESUTE.jpg')
+    if i == 0: await bot.say('https://i.imgur.com/YVfXE7W.gif')
+    elif i == 1: await bot.say('https://i.imgur.com/2SRtfz5.jpg')
+    elif i == 2: await bot.say('https://i.imgur.com/TiESUTE.jpg')
 
 # YEE
-@client.command(pass_context=True)
+@bot.command(pass_context=True)
 async def Yee(ctx):
-    await client.say('https://www.youtube.com/watch?v=q6EoRBvdVPQ')
+    await bot.say('https://www.youtube.com/watch?v=q6EoRBvdVPQ')
 
 # Hard-coded Babooshka
-@client.command(pass_context=True)
+@bot.command(pass_context=True)
 async def babooshka(ctx):
-    await client.say('https://www.youtube.com/watch?v=6xckBwPdo1c')
+    await bot.say('https://www.youtube.com/watch?v=6xckBwPdo1c')
 
 # Hard-coded Where there's a whip, there's a way
-@client.command(pass_context=True)
+@bot.command(pass_context=True)
 async def whip(ctx):
-    await client.say('https://www.youtube.com/watch?v=YdXQJS3Yv0Y')
+    await bot.say('https://www.youtube.com/watch?v=YdXQJS3Yv0Y')
 
 # Hard-coded Moomin Theme Song
-@client.command(pass_context=True)
+@bot.command(pass_context=True)
 async def moomin(ctx):
-    await client.say('https://www.youtube.com/watch?v=oiZ0eBFTH6k')
+    await bot.say('https://www.youtube.com/watch?v=oiZ0eBFTH6k')
 ############################## END MEMES #######################################
 
-client.run('MzQ1NDAwODA0OTY4MTAzOTM3.DG676w.gt_HkXfpCQbxuEwoiHGACywn5Bs')
+bot.run('MzQ1NDAwODA0OTY4MTAzOTM3.DG676w.gt_HkXfpCQbxuEwoiHGACywn5Bs')
 
 #################################################################################
 
 # Notes
 
 # INPUT OF INFORMATION EXAMPLE
-# @client.command(pass_context=True)
+# @bot.command(pass_context=True)
 # async def test(ctx,args):
-#     await client.say('Your text was: {}'.format(args))
+#     await bot.say('Your text was: {}'.format(args))
