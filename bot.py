607c3b7878e2d840521bbdfe4980f2c89d0c8785
Mitch Weaver <mitchweaver@protonmail.com>
2017-08-11 21:59:17 -0500

    message

--- bot.py
+++ bot.py
@@ -60,9 +60,8 @@ async def trans(ctx, args, message):
 # COIN FLIP
 @client.command(pass_context=True)
 async def coinflip(ctx):
-    if random.randint(0, 1):
-        await client.say('Heads')
-    else: await client.say('Tails')
+    await client.say('Heads') if random.randint(0, 1)
+    else await client.say('Tails')
 
 @client.command()
 async def joined(member : discord.Member):
