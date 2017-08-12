21d955c663503f2d4a46d9e3088fb4a05798f026
Mitch Weaver <mitchweaver@protonmail.com>
2017-08-11 23:58:39 -0500

    message

--- bot.py
+++ bot.py
@@ -20,8 +20,8 @@ async def on_ready():
     print("~~~~~~~ bot is starting... ~~~~~~~~~~~~")
 
 #################### FUNCTIONS #################################################
-def send_expiring_message(channel, message, seconds):
-    await client.send_message(channel, message)
+async def send_expiring_message(channel : discord.Channel, message : discord.Message, seconds):
+    await client.send_message(message)
     await client.wait_until_ready()
     await asyncio.sleep(int(seconds)) # how many seconds before deletion
     await client.delete(message)
