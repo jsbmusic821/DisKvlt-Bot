1c5c1cda605b96d17aa5981d4bd16d7113e2f33e
MitchWeaver <mitchweaver@protonmail.com>
2017-08-15 17:20:56 -0500

    asdf

--- bot.py
+++ bot.py
@@ -164,7 +164,7 @@ async def pong(ctx):
 async def restart(ctx):
     author = ctx.message.author
     if str(author.top_role) == "admin":
-        subprocess.call(['$HOME/run.sh'])
+        subprocess.call(['/root/run.sh'])
         await client.say("restarting...")
         exit()
     else: await client.say("Hah, I don't listen to you, @" + str(author))
