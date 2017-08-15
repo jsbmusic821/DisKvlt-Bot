9c34233ad8e6607007d66ab249e200422a8e7b8e
MitchWeaver <mitchweaver@protonmail.com>
2017-08-15 15:26:55 -0500

    asdf

--- bot.py
+++ bot.py
@@ -10,6 +10,7 @@ import lyricfetcher
 import translate
 from translate import Translator
 
+
 des = "Hi, I'm /r/TapeKvlt's bot! Beep, bop, boop..."
 prefix = '!'
 client = commands.Bot(description=des, command_prefix=prefix);
