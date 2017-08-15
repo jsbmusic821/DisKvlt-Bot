c045b6ec25d115b4a41d5228702252aa390fb59d
MitchWeaver <mitchweaver@protonmail.com>
2017-08-15 17:35:47 -0500

    asdf

--- run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Note: 3.6+ is required for discord.py
-PYTHON="python3.6"
-
-# Enter wherever you place your downloaded dir
-BOT_DIR="$HOME/TapeKvlt-Discord-Bot"
-
-cd $BOT_DIR && git pull && $PYTHON bot.py &
