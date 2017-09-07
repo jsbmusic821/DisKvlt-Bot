b22a535685548f5b6bfe4b5afcb08554949424b1
Mitch Weaver <mitchweaver@protonmail.com>
2017-09-07 17:44:39 +0200

    changed greeting message

--- bot.py
+++ bot.py
@@ -43,6 +43,9 @@ async def on_member_join(member):
             i += 1
     if i == 100:
         await client.send_message(server, "https://i.imgur.com/mkolxJC.jpg")
+    elif i < 100:
+        msg = "if you are a previous member, please see this link for more information: http://bit.ly/2wLO9vr"
+        await client.send_message(server, "{0.mention}, " + msg)
 
 
 
